5da8acbab0f293d281195be33d1596fbf171064f
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-10-18 01:24:54 -0700

    feat: Renamed serializer file name

---
+++ api/serializers/user.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import UserManager
+from rest_framework import serializers
+from api.models import TechUser
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = TechUser
+        fields = ['id', 'email', 'identifier_number' ,'first_name', 'last_name', 'gender', 'role']
+
+    def get_role(self, obj):
+        return obj.get_role_display()
