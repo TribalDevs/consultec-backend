c39021c1caeab07dab8fde82e5eebdc795d5f24b
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-10-18 00:49:27 -0700

    feat: Returns user info after login

---
+++ api/views/login.py
@@ -0,0 +1,14 @@
+from api.serializers.login import UserSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+#
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializer(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
