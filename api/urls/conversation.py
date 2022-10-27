340d84e43e311ecf024d02c2883c6bbed51d4735
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-10-27 01:33:00 -0700

    feat: Check for conversation

--- api/urls/conversation.py
+++ api/urls/conversation.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from api.views.conversation import ConversationMessagesView, ValidateConversationView, ConversationsView
+from api.views.conversation import ConversationMessagesView, ConversationValidateView, ValidateConversationView, ConversationsView
 
 urlpatterns = [
     path("active/", ConversationsView.as_view(), name="Conversation"),
+    
     path("<str:convo_id>/messages/", ConversationMessagesView.as_view(), name="Conversation Messages"),
+
     path("<str:user_id>/new/", ValidateConversationView.as_view(), name="Post Message"),
+    
+    path("<str:user_id>/messages/", ConversationValidateView.as_view(), name="Validate Messages"),
 ]
