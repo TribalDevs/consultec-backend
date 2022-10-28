1459958d3f5da66f54fe9cba5de41210e66ae08a
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-10-28 00:33:10 -0700

    reformat: Removed useless imports

--- api/urls/sign_up.py
+++ api/urls/sign_up.py
@@ -1,7 +1,3 @@
-from distutils.command.sdist import sdist
-from ossaudiodev import SOUND_MIXER_CD
-from ssl import ALERT_DESCRIPTION_DECODE_ERROR
-from stat import SF_APPEND
 from django.urls import path
 from api.views.sign_up import SignUpView
 
