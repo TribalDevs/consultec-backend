a4ae7880d88166f8147b386ede1d3780602fc566
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-12-02 00:32:41 -0800

    feat: Admin creation

--- api/urls/sign_up.py
+++ api/urls/sign_up.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views.sign_up import SignUpView
+from api.views.sign_up import SignUpView, SignUpAdminView
 
 urlpatterns = [
-    path('', SignUpView.as_view())
+    path('', SignUpView.as_view()),
+    path('admin/', SignUpAdminView.as_view()),
 ]
